4a3425c070ae4d897810fa7fdcfbc80e0518324e
Alethea Cary <aletheacary@gmail.com>
2019-05-06 05:43:01 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -10,7 +10,7 @@ end
 
 def first_element
   @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-  @taylor_swift.firt
+  @taylor_swift.first
 end
 
 def third_element

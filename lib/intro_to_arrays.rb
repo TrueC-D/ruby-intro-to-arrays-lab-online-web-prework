1b41f8fb692803c8290a19ce6633ec6427b200ce
Alethea Cary <aletheacary@gmail.com>
2019-05-06 05:35:47 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,33 @@
+def instantiate_new_array
+  instantiate_new_array = @my_new_array
+  @my_new_array = []
+  @my_new_array
+end
+
+def array_with_two_elements
+  
+end
+
+def first_element
+  
+end
+
+def third_element
+  
+end
+
+def last_element
+  
+end
+
+def first_element_with_array_methods
+  
+end
+
+def last_element_with_array_methods
+  
+end
+
+def length_of_array
+  
+end

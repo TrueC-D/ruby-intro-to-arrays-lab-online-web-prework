f13369da3eab5b0b219d45ab770347bc1cd636b7
Alethea Cary <aletheacary@gmail.com>
2019-05-06 05:57:28 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -21,11 +21,11 @@ def last_element (array)
 end
 
 def first_element_with_array_methods
-  my_first_element = array.first
+  first_country = array.first
 end
 
 def last_element_with_array_methods
-  my_last_element = array.last
+  last_country = array.last
 end
 
 def length_of_array
